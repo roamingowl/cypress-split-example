3e4fa69f4d781f6b4a0de0e0403a3737cef79d48
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2024-08-13 16:43:39 +0200

    chore: remove lodash as dependency

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,28 +1,36 @@
 import { defineConfig } from "cypress";
 import fg from 'fast-glob';
-import chunk from 'lodash/chunk';
 
 function splitTests(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
-  const splitChunks = parseInt(config.env.SPLIT);
-  const splitChunk = parseInt(config.env.SPLIT_INDEX);
-  console.log(`Validating split params. Splits: ${splitChunks}, split index: ${splitChunk}`);
-  if (splitChunks > 1 && splitChunk > 0 && config.env.SPEC) {
-    if (splitChunk > splitChunks) {
+  const chunksCount = parseInt(config.env.SPLIT);
+  const currentChunk = parseInt(config.env.SPLIT_INDEX);
+  const specPattern = config.env.SPEC;
+  console.log(`Validating split params. Splits: ${chunksCount}, split index: ${currentChunk}, specs: ${specPattern}`);
+  if (chunksCount > 1 && currentChunk > 0 && specPattern) {
+    if (currentChunk > chunksCount) {
         throw new Error(
-            `Invalid split index ${splitChunk}. It should be less than or equal to total splits ${splitChunks}`
+            `Invalid split index ${currentChunk}. It should be less than or equal to total splits ${chunksCount}`
         );
     }
     console.log(
-        `Splitting is enabled. Chunks requested: ${splitChunks}. Now processing  ${splitChunk}`
+        `Splitting is enabled. Chunks requested: ${chunksCount}. Now processing  ${currentChunk}`
     );
-    console.log(`Spec pattern: ${config.env.SPEC}`);
+    console.log(`Spec pattern: ${specPattern}`);
     const specs = fg.sync(config.specPattern).sort();
+    //in node 22+ it will be possible to use native version import { glob } from 'node:fs';
     console.log(`Spec pattern matches total of ${specs.length} specs.`);
-    const chunkSize = Math.ceil(specs.length / splitChunks);
-    console.log(`Splitting into ${splitChunks} chunks of avg size: ${chunkSize} specs.`);
-    const chunks = chunk(specs, chunkSize);
-    config.specPattern = chunks[splitChunk - 1];
-    console.log(`Setting new spec pattern: ${chunks[splitChunk - 1].join('\n')}`);
+    const chunkSize = Math.ceil(specs.length / chunksCount);
+    console.log(`Splitting into ${chunksCount} chunks of size ${chunkSize} or less.`);
+    let chunks: string[][] = [];
+    let i = 0;
+
+    while (i < specs.length) {
+      chunks.push(specs.slice(i, i += chunkSize));
+    }
+
+    // const chunks = chunk(specs, chunkSize);
+    config.specPattern = chunks[currentChunk - 1];
+    console.log(`Setting new spec pattern (${chunks[currentChunk - 1].length}): ${chunks[currentChunk - 1].join('\n')}`);
   } else {
     console.log(`Splitting is disabled. Using specified pattern: ${config.specPattern}`);
   }
