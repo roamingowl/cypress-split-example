91b84a984bcf302f9e3d723492b3a894994a383a
Juraj Šimon <118170619+simonjur@users.noreply.github.com>
2024-09-04 09:24:31 +0200

    chore: add some explaining comments

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,25 +1,32 @@
 import { defineConfig } from "cypress";
+//in node 22+ it will be possible to use native version import { glob } from 'node:fs';
 import fg from 'fast-glob';
 import { GenerateCtrfReport } from 'cypress-ctrf-json-reporter';
 
 function splitTests(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
+  // load control variables
   const chunksCount = parseInt(config.env.SPLIT);
   const currentChunk = parseInt(config.env.SPLIT_INDEX);
   const specPattern = config.env.SPEC;
+
   console.log(`Validating split params. Splits: ${chunksCount}, split index: ${currentChunk}, specs: ${specPattern}`);
   if (chunksCount > 1 && currentChunk > 0 && specPattern) {
+    // validate numeric variables
     if (currentChunk > chunksCount) {
         throw new Error(
             `Invalid split index ${currentChunk}. It should be less than or equal to total splits ${chunksCount}`
         );
     }
     console.log(
-        `Splitting is enabled. Chunks requested: ${chunksCount}. Now processing  ${currentChunk}`
+        `Splitting is enabled. Chunks count requested: ${chunksCount}. Now processing  ${currentChunk}`
     );
-    console.log(`Spec pattern: ${specPattern}`);
-    const specs = fg.sync(config.specPattern).sort();
+
     //in node 22+ it will be possible to use native version import { glob } from 'node:fs';
+    //resolve all the spec files that match the pattern
+    const specs = fg.sync(config.specPattern).sort();
     console.log(`Spec pattern matches total of ${specs.length} specs.`);
+
+    //split the specs into chunks of the same size (except the last one)
     const chunkSize = Math.ceil(specs.length / chunksCount);
     console.log(`Splitting into ${chunksCount} chunks of size ${chunkSize} or less.`);
     let chunks: string[][] = [];
@@ -29,7 +36,7 @@ function splitTests(on: Cypress.PluginEvents, config: Cypress.PluginConfigOption
       chunks.push(specs.slice(i, i += chunkSize));
     }
 
-    // const chunks = chunk(specs, chunkSize);
+    //change the spec pattern to the exact files list in the current chunk
     config.specPattern = chunks[currentChunk - 1];
     console.log(`Setting new spec pattern (${chunks[currentChunk - 1].length}): ${chunks[currentChunk - 1].join('\n')}`);
   } else {
@@ -40,8 +47,10 @@ function splitTests(on: Cypress.PluginEvents, config: Cypress.PluginConfigOption
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      //Splitting is done here. The following function is like our own micro-splitting plugin.
       splitTests(on, config);
 
+      //JSON reporter - not needed for splitting itself, but handy to display merged results later
       new GenerateCtrfReport({
         on,
         outputFile: config.env.SPLIT_INDEX ? `ctrf-report-${config.env.SPLIT_INDEX}.json` : 'ctrf-report.json',
